a2072fc1badef2cef51ad410daebe924e049d073
minghanminghan <andrewjiang6789@gmail.com>
2024-09-25 00:40:42 -0400

    placeholder website

--- frontend/my-component.js
+++
@@ -1,9 +0,0 @@
-// my-component.js
-import { ref } from 'vue'
-export default {
-  setup() {
-    const count = ref(0)
-    return { count }
-  },
-  template: `<div>Count is: {{ count }}</div><button @click="count++">Add 1</button>`
-}
